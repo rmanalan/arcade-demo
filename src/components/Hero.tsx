80158626a2c674a394d68caa2aba04d095664655
Darshan Desai <darshandesai17@gmail.com>
2021-01-15 04:45:14 -0500

    Update Copy

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,8 +21,8 @@ export function Hero({ demoImgUrl }: { demoImgUrl?: string }) {
             <div className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6">
               <div className="text-center">
                 <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                  <span className="block">Arcade helps you</span>
-                  <span className="block text-indigo-600">convert better</span>
+                  <span className="block">Capture your product</span>
+                  <span className="block text-indigo-600">in action</span>
                 </h1>
                 <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
                   Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
